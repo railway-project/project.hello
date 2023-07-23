a493fcc3ed7c925140a6bb357934d64798a4c1b0
railway-project <shivkumargupt13100@gmail.com>
2023-07-23 19:01:22 +0530

    first commit:

---
+++ myproject/poems/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.utils.text import slugify
+
+
+class Poem(models.Model):
+    CATEGORY_CHOICES = [
+        ('love', 'Love'),
+        ('motivational', 'Motivational'),
+        ('sad', 'Sad'),
+        # Add more categories will add soon 
+    ]
+
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    content = models.TextField()
+    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, default='uncategorized')
+    thumbnail = models.ImageField(upload_to='poem_thumbnails/', null=True, blank=True)
+    authors_image = models.ImageField(upload_to='poem_images/', default='rishi-image.jpg')
+    main_image = models.ImageField(upload_to='poem_images/', null=True, blank=True)
+    is_new = models.BooleanField(default=True)
+    slug = models.SlugField(unique=True, blank=True)
+    views = models.IntegerField(default=0)
+    publish_date = models.DateField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:  # Check if the slug is empty
+            self.slug = slugify(self.title)  # Generate the slug if it is empty
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    poem = models.ForeignKey(Poem, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.body, self.name)
---
+++ myproject/poems/views.py
@@ -0,0 +1,76 @@
+from django.urls import reverse
+
+from django.contrib import messages
+from django.shortcuts import redirect
+from django import forms
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Poem,Comment
+from .form import CommentForm
+def search(request):
+    query = request.GET.get('query', '')
+    poems = Poem.objects.filter(content__icontains=query) | Poem.objects.filter(author__icontains=query)
+    context = {
+        'query': query,
+        'results': poems,
+        'selected_category': '',  
+    }
+    return render(request, 'index.html', context=context)
+
+def home(request):
+    categories = Poem.objects.values_list('category', flat=True).distinct()
+    selected_category = request.GET.get('category')
+    query = request.GET.get('query', '')
+
+    if selected_category:
+        poems = Poem.objects.filter(category=selected_category)
+    else:
+        poems = Poem.objects.filter(is_new=True)  # Only fetch new poems for the main page
+
+    context = {
+        'poems': poems,
+        'selected_category': selected_category,
+        'categories': categories,
+        'query': query,
+    }
+    return render(request, 'index.html', context=context)
+def poem_detail(request, slug, pk):
+    poems = []  # List to store multiple poems
+    poems.append(get_object_or_404(Poem, slug=slug, pk=pk))
+
+    context = {
+        'poems': poems,
+    }
+    return render(request, 'rishi_poems.html', context=context)
+
+def post_detail(request, pk, slug):
+    poem = get_object_or_404(Poem, pk=pk, slug=slug)
+    comments = Comment.objects.filter(poem=poem, active=True)
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.poem = poem
+            new_comment.save()
+            return redirect('poem-detail', pk=pk, slug=slug)
+    else:
+        comment_form = CommentForm()
+
+    context = {
+        'poem': poem,
+        'comments': comments,
+        'new_comment': new_comment,
+        'comment_form': comment_form,
+    }
+    return render(request, 'post_detail.html', context=context)
+def view_system(request, slug, pk):
+    poem = get_object_or_404(Poem, pk=pk, slug=slug)
+    poem.views += 1
+    poem.save()
+    context = {
+        'poem': poem,
+    'views': poem.views
+    }
+    return render(request, 'rishi_poems.html', context)
